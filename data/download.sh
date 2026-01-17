5cadc97c06032c473ab30d050bb0a1043eb9aead
FabulousCupcake <mail.fabulous.cupcake@gmail.com>
2026-01-17 11:57:48 +0100

    Print downloaded json into GITHUB_STEP_SUMMARY

--- data/download.sh
+++ data/download.sh
@@ -17,16 +17,26 @@ map({
 }) |
 add'
 
+debug() {
+  FILE_PATH="$1"
+  echo '```json' >> "$GITHUB_STEP_SUMMARY"
+  cat "$FILE_PATH" >> "$GITHUB_STEP_SUMMARY"
+  echo '```' >> "$GITHUB_STEP_SUMMARY"
+}
+
 # Weapons
 printf "Weapons... "
 WEAPONS_URL='https://gbf.wiki/index.php?title=Special:CargoExport&tables=weapons%2C+_pageData%2C&join+on=weapons._pageName%3D_pageData._pageName&fields=weapons._pageName%2C+_pageData._pageName%2C+_pageData._modificationDate&&order+by=`cargo___pageData`.`_pageName`%2C`cargo___pageData`.`_modificationDate`&limit=5000&format=json';
 curl -A "$USER_AGENT" -fsSL "$WEAPONS_URL&$RANDOM" > "$TEMP_FILE"
+debug "$TEMP_FILE"
 jq "$JQ_QUERY" "$TEMP_FILE" > "$WEAPONS_FILE"
 echo "OK $(cat "$WEAPONS_FILE" | wc -l)"
+
 
 # Summons
 printf "Summons... "
 SUMMONS_URL='https://gbf.wiki/index.php?title=Special:CargoExport&tables=summons%2C+_pageData%2C&join+on=summons._pageName%3D_pageData._pageName&fields=summons._pageName%2C+_pageData._pageName%2C+_pageData._modificationDate&&order+by=`cargo___pageData`.`_pageName`%2C`cargo___pageData`.`_modificationDate`&limit=5000&format=json';
 curl -A "$USER_AGENT" -fsSL "$SUMMONS_URL&$RANDOM" > "$TEMP_FILE"
+debug "$TEMP_FILE"
 jq "$JQ_QUERY" "$TEMP_FILE" > "$SUMMONS_FILE"
 echo "OK $(cat "$SUMMONS_FILE" | wc -l)"
